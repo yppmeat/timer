742a757b075abf709feb59fd2c72f9f0ea9efb73
yppmeat <149466893+yppmeat@users.noreply.github.com>
2024-04-12 19:06:58 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -19,11 +19,34 @@ const length = {
 let minutes = 0, seconds = 0;
 let temp;
 let sumSeconds = 0;
-let startedTime;
+let startedTime = null;
+let elapsedTime = null;
 let radix, len;
+let round = 0;
 
 setRadix(2);
 render();
+
+$select.innerHTML = [...Array(15)].map((_, i) => {
+  return /*html*/`
+    <label>
+      ${i + 2}進数
+      <input type="radio" name="radix" value="${i + 2}" ${i == 0 ? 'checked' : ''}>
+    </label>
+  `
+}).join('');
+
+$screen.addEventListener('click', () => {
+  $select.classList.add('show');
+});
+
+$select.querySelectorAll('label').forEach(v => {
+  v.addEventListener('click', () => {
+    setRadix(+v.querySelector('input').value);
+    render();
+    $select.classList.remove('show');
+  });
+});
 
 $plus10m.addEventListener('click', () => {
   add(10, 0);
@@ -39,6 +62,7 @@ $plus10s.addEventListener('click', () => {
 
 $reset.addEventListener('click', () => {
   minutes = seconds = 0;
+  stopTimer();
   render();
 });
 
@@ -49,6 +73,9 @@ $start.addEventListener('click', () => {
 function setRadix(r) {
   radix = r;
   len = length[r];
+  $title.innerText = document.title = r + '進数タイマー';
+  $plus10m.value = (10).toString(r).toUpperCase() + '分';
+  $plus10s.value = (10).toString(r).toUpperCase() + '秒';
   $screen.classList.remove('digit2', 'digit3', 'digit4-');
   $screen.classList.add(len == 2 ? 'digit2' : len == 3 ? 'digit3' : 'digit4-');
 }
@@ -74,6 +101,25 @@ function pad(n, len) {
 }
 
 function startTimer() {
+  if(startedTime) {
+    if(round == 1) {
+      elapsedTime = (performance.now() - startedTime);
+      startedTime = null;
+      $start.value = 'スタート';
+      round = 3;
+    } else if(round == 2) {
+      stopTimer();
+      render();
+    }
+    return;
+  }
+  if(round == 3) {
+    startedTime = performance.now() - elapsedTime;
+    $start.value = 'ストップ';
+    round = 1;
+    frame();
+    return;
+  }
   $plus10m.disabled =
     $plus1m.disabled =
     $plus10s.disabled = true;
@@ -81,10 +127,12 @@ function startTimer() {
   sumSeconds = minutes * 60 + seconds;
   temp = [minutes, seconds];
   startedTime = performance.now();
+  round = 1;
   frame();
 }
 
 function frame() {
+  if(!startedTime) return;
   const remaining = sumSeconds - (performance.now() - startedTime) / 1000;
   if(remaining >= 0) {
     minutes = remaining / 60 | 0;
@@ -94,10 +142,29 @@ function frame() {
   } else {
     minutes = seconds = 0;
     render();
+    startedTime = performance.now();
+    round = 2;
     finishTimer();
   }
 }
 
 function finishTimer() {
-  
+  if(!startedTime) {
+    $screen.classList.remove('hide');
+    return;
+  }
+  const show = (performance.now() - startedTime) / 500 % 2 | 0;
+  $screen.classList.toggle('hide', !show);
+  requestAnimationFrame(finishTimer);
+}
+
+function stopTimer() {
+  if(!startedTime && round != 3) return;
+  round = 0;
+  $plus10m.disabled =
+    $plus1m.disabled =
+    $plus10s.disabled = false;
+  $start.value = 'スタート';
+  [minutes, seconds] = temp;
+  startedTime = null;
 }
